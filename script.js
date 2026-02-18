ff9232fab3f44a440fc77de92aee7c1162f362f5
cobalbusiness1-png <cobal.business1@gmail.com>
2026-02-19 08:21:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,19 @@ const menu = document.querySelector(".main-menu");
 
 window.addEventListener("load", () => {
 
-    const length = text.getBBox().width * 1.1;
+    const length = text.getBBox().width;
 
     text.style.strokeDasharray = length;
     text.style.strokeDashoffset = length;
 
-    /* ペン描画 */
-    text.style.transition = "stroke-dashoffset 2.8s ease";
+    text.style.transition = "stroke-dashoffset 2.5s ease";
+
     requestAnimationFrame(() => {
         text.style.strokeDashoffset = "0";
     });
 
-    /* 描画後スライド */
     setTimeout(() => {
 
-        logo.style.transition = "transform 1.2s ease, opacity 1.2s ease";
         logo.style.transform = "translateY(-120px)";
         logo.style.opacity = "0";
 
@@ -26,6 +24,6 @@ window.addEventListener("load", () => {
             menu.classList.add("show");
         }, 1200);
 
-    }, 3000);
+    }, 2600);
 
 });

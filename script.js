e2c7c906172281e63ef286c5a8571fcda6cfc58e
cobalbusiness1-png <cobal.business1@gmail.com>
2026-02-19 08:10:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+const logoStroke = document.querySelector('.logo-stroke');
 const logoFill = document.querySelector('.logo-fill');
+const logo = document.getElementById('cobal-logo');
 const menu = document.querySelector('.main-menu');
 
+/* 塗りアニメ終了後にロゴを上へスライド */
 logoFill.addEventListener('animationend', (e) => {
-    if (e.animationName === "moveUpFade") {
-        menu.classList.add('show');
+
+    if (e.animationName === "fillFade") {
+
+        // 少し待ってから上へ移動
+        setTimeout(() => {
+            logo.classList.add('logo-exit');
+
+            // ロゴ移動後にメニュー表示
+            setTimeout(() => {
+                menu.classList.add('show');
+            }, 1500);
+
+        }, 800);
     }
 });
 

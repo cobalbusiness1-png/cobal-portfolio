27727cdd502ec65df5a6d0e46e9172a3040a1b0f
cobalbusiness1-png <cobal.business1@gmail.com>
2026-02-19 07:57:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-// アニメーションはCSSで制御しています。
+const logoFill = document.querySelector('.logo-fill');
+const menu = document.querySelector('.main-menu');
+
+logoFill.addEventListener('animationend', (e) => {
+    if (e.animationName === "moveUpFade") {
+        menu.classList.add('show');
+    }
+});
+
 console.log("Cobal Studio Animation System Active.");
